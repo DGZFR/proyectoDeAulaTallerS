6cfc6259d89d9fc52904fe77ace8e6fe128aff02
jramirdo <53876581+jramirdo@users.noreply.github.com>
2020-05-21 13:21:05 -0500

    archivo necesario para validacion de inicio
    necesario para poder validar si el codigo de mi app  funciona

--- constants/check-login.php
+++ constants/check-login.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
 if (isset($_SESSION['logged']) && $_SESSION['logged'] == true) {
-$user_online = true;	
+$myid = $_SESSION['myid'];
+$mylogin = $_SESSION['lastlogin'];
 $myrole = $_SESSION['role'];
+	
+$user_online = true;	
 }else{
 $user_online = false;
 }
